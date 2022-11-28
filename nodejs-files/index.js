32e0818e377012489e62a34e483af8535fc39eb9
Patrick Wright <patrickdane@me.com>
2022-11-28 09:33:16 -0500

    node file management

--- nodejs-files/index.js
+++ nodejs-files/index.js
@@ -1,4 +1,17 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function calculateSalesTotal(salesFiles) {
+  let salesTotal = 0;
+  // loop over each file path in the salesFiles array
+  for (file of salesFiles) {
+    // read the file and parse the contents as JSON
+    const data = JSON.parse(await fs.readFile(file));
+    // add the amount in the data.total field to the salesTotal variable
+    salesTotal += data.total;
+  }
+  return salesTotal;
+}
 
 async function findSalesFiles(folderName) {
   // this array will hold sales files as they are found
@@ -6,19 +19,19 @@ async function findSalesFiles(folderName) {
 
   async function findFiles(folderName) {
     // read all the items in the current folder
-    const items = await fs.readdirSync(folderName, { withFileTypes: true });
+    const items = await fs.readdir(folderName, { withFileTypes: true });
 
     // iterate over each found item
     for (item of items) {
       // if the item is a directory, it will need to be searched for files
       if (item.isDirectory()) {
         // search this directory for files (this is recursion!)
-        await findFiles(`${folderName}/${item.name}`);
+        await findFiles(path.join(folderName, item.name));
       } else {
         // Make sure the discovered file is a sales.json file
-        if (item.name === "sales.json") {
+        if (path.extname(item.name) === ".json") {
           // store the file path in the salesFiles array
-          salesFiles.push(`${folderName}/${item.name}`);
+          await salesFiles.push(path.join(folderName, item.name));
         }
       }
     }
@@ -32,8 +45,32 @@ async function findSalesFiles(folderName) {
 }
 
 async function main() {
-  const salesFiles = await findSalesFiles("stores");
-  console.log(salesFiles);
+  // variable to store a path to the stores directory
+  const salesDir = path.join(__dirname, "stores");
+  // variable to store the path of the salesTotals directory
+  const salesTotalsDir = path.join(__dirname, "salesTotals");
+
+  // create salesTotal directory if it doesn't exist
+  try {
+    await fs.mkdir(salesTotalsDir);
+  } catch {
+    console.log(`${salesTotalsDir} already exists.`);
+  }
+
+  // find paths to all the sales files
+  const salesFiles = await findSalesFiles(salesDir);
+
+  // read through each sales file to calculate the sales total
+  const salesTotal = await calculateSalesTotal(salesFiles);
+
+  // write the total to the "totals.json" file
+  await fs.writeFile(path.join("salesTotals/totals.txt"), `${salesTotal}\r\n`, {
+    flag: "a"
+  });  
 }
 
 main();
+// console.log(path.extname("sales.json")); // .json
+// console.log(path.parse("stores/201/sales.json"));
+// // { root: '', dir: 'stores/201', base: 'sales.json', ext: '.json', name: 'sales' }
+
