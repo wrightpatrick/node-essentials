a88358089e9b5dac1955bfa0f33508d7e5d68e56
Patrick Wright <patrickdane@me.com>
2022-11-28 12:30:13 -0500

    API CRUD implementation

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -7,21 +7,30 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // implement
-});
-
-app.put('/products', function (req, res) {
-  // implement
-});
-
-app.delete('/products/:id', function (req, res) {
-  // implement
-});
-
-app.get('/products', (req, res) => {
-  // implement
-})
+app.route('/products')
+ .get((req, res) => {
+   res.json(products);
+ })
+ .post((req, res) => {
+   const newProduct = { ...req.body, id: products.length + 1 }
+   products = [...products, newProduct]
+   res.json(newProduct);
+ })
+.put((req, res) => {
+   let updatedProduct;
+   products = products.map(p => {
+     if (p.id === req.body.id) {
+       updatedProduct = { ...p, ...req.body };
+       return updatedProduct;
+     }
+     return p;
+   })
+   res.json(updatedProduct);
+ })
+ .delete((req, res) => {
+   const deletedProduct = products.find(p => p.id === +req.body.id);
+   products = products.filter(p => p.id !== +req.body.id);
+   res.json(deletedProduct);
+ })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  
